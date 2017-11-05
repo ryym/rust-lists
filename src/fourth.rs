0a07bbab65126306c8d37c9749778cb030359df3
ryym <ryym.64@gmail.com>
2017-11-05 18:00:08 +0900

    Bad Safe Dequeue: Implement initializers

--- src/fourth.rs
+++ src/fourth.rs
@@ -18,3 +18,19 @@ struct Node<T> {
     next: Link<T>,
     prev: Link<T>,
 }
+
+impl<T> Node<T> {
+    fn new(elem: T) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Node {
+            elem,
+            prev: None,
+            next: None,
+        }))
+    }
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List { head: None, tail: None }
+    }
+}
