2584e8c56213789c72f9d085f93ca0cf74b858d3
ryym <ryym.64@gmail.com>
2017-11-05 10:12:48 +0900

    Persistent Stack: Implement Iter trait

--- src/third.rs
+++ src/third.rs
@@ -38,6 +38,28 @@ impl<T> List<T> {
     }
 }
 
+pub struct Iter<'a, T: 'a> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<T> List<T> {
+    pub fn iter(&self) -> Iter<T> {
+        Iter { next: self.head.as_ref().map(|node| &**node) }
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_ref().map(|node| &**node);
+            &node.elem
+        })
+    }
+}
+
+// 状態変更をしない実装のため、`third::List`の`Iter`や`IterMut`は実装できない。
+
 #[cfg(test)]
 mod test {
     use super::List;
@@ -53,5 +75,15 @@ mod test {
         assert_eq!(list.tail().tail().head(), Some(&1));
         assert_eq!(list.tail().tail().tail().head(), None);
         assert_eq!(list.tail().tail().tail().tail().head(), None);
+    }
+
+    #[test]
+    fn iter() {
+        let list = List::new().append(1).append(2);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
+        assert_eq!(list.head(), Some(&2));
     }
 }
