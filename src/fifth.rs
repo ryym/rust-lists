f926dfd54571de80b24dceab8abf03a44a70f278
ryym <ryym.64@gmail.com>
2017-11-08 22:51:13 +0900

    Unsafe Queue: Add note about implementation difficulty

--- src/fifth.rs
+++ src/fifth.rs
@@ -1,3 +1,16 @@
+// - singly-linked queue を実装したい。
+// - Listが`head`しか持ってないと、 push や pop に O(n) かかる
+//   (毎回最後のノードまで辿らないといけない)
+// - `List`が最後のノードも持ってればいい!
+// - 所有権の問題があるため、`tail: Link<T>`とは書けない。
+//   (最後のノードはその手前のノードの`next`が所有する)
+// - `tail: Option<&mut Node<T>>`と書いて参照を保持しよう!
+// - その参照の lifetimeは? ...Listと同じ?
+//   (`List<'a, T: 'a> { tail: Option<&'a mut Node<T>>, head: Link<T> }`)
+// - しかしこれは間違っている。`List`が生きている限り、
+//   本当は既に無効な参照を`tail`が持ち続けてしまう可能性がある。
+// - このlifetimeを適切に設定する方法がない..?
+
 pub struct List<T> {
     head: Link<T>,
     tail: Link<T>,
