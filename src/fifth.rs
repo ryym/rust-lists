d97cdd405e8f141a0357b3d6057a5ff4caa92992
ryym <ryym.64@gmail.com>
2017-11-08 22:50:19 +0900

    Unsafe Queue: set up

---
+++ src/fifth.rs
@@ -0,0 +1,17 @@
+pub struct List<T> {
+    head: Link<T>,
+    tail: Link<T>,
+}
+
+type Link<T> = Option<Box<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List { head: None, tail: None }
+    }
+}
